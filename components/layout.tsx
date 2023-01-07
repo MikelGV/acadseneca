3e0da1f13079da31b0bf309bddbc96ce333635db
Mikel <mikelgaldosv@gmail.com>
2023-01-07 18:19:46 +0100

    first setup

---
+++ components/layout.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import TopBar from "./topbar";
+import Footer from "./footer";
+
+export default function Layout({children, home}: {children:any, home: boolean}) {
+    return(
+        <div>
+            <Head>
+                <link rel="icon" href="/favicon.ico"/>
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap');
+                </style> 
+            </Head>
+            <header>
+                <TopBar />
+            </header>
+            <main>{children}</main>
+            <Footer />
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>test</h1>
+    <Layout home>
+      <h1>this is main page</h1>
+    </Layout>
   )
 }
