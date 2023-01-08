f42be809839e412f0094e17951b3e46ec30fab5b
Mikel <mikelgaldosv@gmail.com>
2023-01-08 07:01:06 +0100

    added footer content

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,26 @@
+import style from "../footer/footer.module.css"
+
 export default function Footer() {
     return(
-        <h1>this is footer</h1>
+        <div className={style.footer}>
+            <div className={style.footerWrapper}>
+                <div className={style.left}>
+                    <h1 className={style.leftTitle}>Sobre Nosotros</h1>
+                    <p className={style.leftText}>
+                        Desde hace más de 30 años, la filosofía de Academia Seneca queda resumida en nuestro lema "El verdadero protagonista es el alumno".
+                    </p>
+                </div>
+                <div className={style.right}>
+                    <h1 className={style.rightTitle}>Dónde Estamos</h1>
+                    <p className={style.rightText}>
+                        Localización 1:
+                            Los Hoyos Bidea, 9, 48980 Santurtzi, Bizkaia
+                            Telefono: 94 471 46 10
+                        Localización 2:
+                            P.º Blasco Ibanez Páseálekyá, 1 BIS, 48500 Gallarta
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
