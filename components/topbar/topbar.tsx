b65bfb9b3bc2ecbaab28d4d232cd1bda94b605ff
Mikel <mikelgaldosv@gmail.com>
2023-01-08 02:41:00 +0100

    added classes to topbar

--- components/topbar/topbar.tsx
+++ components/topbar/topbar.tsx
@@ -1,19 +1,20 @@
-import Link from "next/link"
+import Link from "next/link";
+import styles from "../topbar/topbar.module.css";
 
 export default function TopBar() {
     return(
-        <div className="topbar">
-            <div className="topbarWrapper">
-                <div className="left">
-                    <div className="leftWrapper">
-                        <h1 className="logo">Academia Seneca</h1>
+        <div className={styles.topbar}>
+            <div className={styles.topbarWrapper}>
+                <div className={styles.left}>
+                    <div className={styles.leftWrapper}>
+                        <Link className={styles.logo} href="/">Academia Seneca</Link>
                     </div>
                 </div>
-                <div className="right">
-                    <div className="rightWrapper">
-                        <Link href="/dondeEstamos" className="">Donde Estamos</Link>
-                        <Link href="quienesSomos" className="">Quienes Somos</Link>
-                        <Link href="contact" className="">Contacto</Link>
+                <div className={styles.right}>
+                    <div className={styles.rightWrapper}>
+                        <Link href="/dEstamos" className={styles.dEstamos}>Donde Estamos</Link>
+                        <Link href="/qSomos" className={styles.qSomos}>Quienes Somos</Link>
+                        <Link href="/contact" className={styles.contact}>Contacto</Link>
                     </div>
                 </div>
             </div>
