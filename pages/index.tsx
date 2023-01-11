f2ff9551f272dc5ee11c9395b3c61d2667353ce5
Mikel <mikelgaldosv@gmail.com>
2023-01-11 03:35:13 +0100

    changed to capital letters

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <img className={styles.mainImg} src="/img/optiona.jpg" alt="" />
             <div className={styles.centered}>
               <h1 className={styles.centeredLogo}>Academia Seneca</h1>
-              <h2 className={styles.centeredTitle}>Diviértete Estudiando</h2>
+              <h2 className={styles.centeredTitle}>DIVIÉRTETE ESTUDIANDO</h2>
               <p className={styles.centeredText}>
                 Clases particulares adaptadas a las necesidades de cada alumno
                 Refuerzo escolar personalizado y preparación para los examense oficiales de idiomas
