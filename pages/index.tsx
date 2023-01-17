eda11efb599a04cb2cadde4d10a6e77e86df2e98
Mikel <mikelgaldosv@gmail.com>
2023-01-17 01:46:31 +0100

    first stage of the courses added

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,27 +37,35 @@ export default function Home() {
           </div>
 
           <div className={styles.coursesWrapper}>
+
             <div className={styles.primaria}>
               <img className={styles.pImg} src="/img/primaria.jpg" alt="" />
-              <h2 className={styles.pText}></h2>
+              <p className={styles.pText}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet adipisci quo architecto iure, ut quibusdam libero, nemo pariatur alias esse exercitationem in illo voluptas corrupti doloremque dolores ipsam. Velit, accusamus!</p>
             </div>
+
             <div className={styles.eso}>
               <img className={styles.eImg} src="/img/eso.jpg" alt="" />
-              <h2 className={styles.eText}></h2>
+              <p className={styles.eText}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus veritatis ratione impedit exercitationem expedita voluptas dolorem fugiat nisi at. Saepe labore necessitatibus ad iure aut suscipit ea sed recusandae provident?</p>
             </div>
+
             <div className={styles.bachiller}>
               <img className={styles.bImg} src="/img/bachiller.jpg" alt="" />
-              <h2 className={styles.bText}></h2>
+              <p className={styles.bText}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis non mollitia quod, aperiam ut deleniti nam magni consequuntur repellat earum voluptas sequi quia suscipit laboriosam veniam doloribus? Porro, labore accusamus!</p>
             </div>
-            <div className={styles.uni}>
-              <img className={styles.uImg} src="/img/uni.jpg" alt="" />
-              <h2 className={styles.uText}></h2>
-            </div>
-            <div className={styles.idiomas}>
-              <img className={styles.iImg} src="/img/language.jpg" alt="" />
-              <h2 className={styles.iText}></h2>
-            </div>
+
           </div>
+
+          <div className={styles.bottomGrid}>
+              <div className={styles.uni}>
+                <img className={styles.uImg} src="/img/uni.jpg" alt="" />
+                <p className={styles.uText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis nihil aliquam doloremque corporis, veritatis repudiandae dolorum, fugit perferendis ipsum aspernatur architecto itaque quia sequi assumenda sint vel fuga veniam?</p>
+              </div>
+
+              <div className={styles.idiomas}>
+                <img className={styles.iImg} src="/img/language.jpg" alt="" />
+                <p className={styles.iText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis ut aperiam. Voluptatibus fugiat sapiente aperiam sequi pariatur earum eius officia, omnis libero, iste cum inventore voluptatum voluptates magni. Vero.</p>
+              </div>
+            </div>
       </div>
     </Layout>
   )
