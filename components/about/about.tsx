8fa50ff96faf263d405790210538275e68d4aba4
Mikel <mikelgaldosv@gmail.com>
2023-01-21 00:51:58 +0100

    about component added

---
+++ components/about/about.tsx
@@ -0,0 +1,16 @@
+import styles from '../about/about.module.css';
+
+export default function About() {
+    return(
+        <div className={styles.aboutWrapper}>
+            <div className={styles.aboutLeft}>
+              <img className={styles.leftImg} src="/img/aboutTheFormationOfGroups.jpg" alt="" />
+              <p className={styles.leftText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis, minima ipsum dolore in, pariatur quaerat placeat error inventore natus dolor harum! Debitis recusandae consequuntur earum! Hic odio saepe in?</p>
+            </div>
+            <div className={styles.aboutRight}>
+              <img className={styles.rightImg} src="/img/aboutTheTeachers.jpg" alt="" />
+              <p className={styles.rightText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis, minima ipsum dolore in, pariatur quaerat placeat error inventore natus dolor harum! Debitis recusandae consequuntur earum! Hic odio saepe in?</p>
+            </div>
+        </div>
+    )
+}
