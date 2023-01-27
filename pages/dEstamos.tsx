2c6ecc2a0a38f1cb96a337d50d1406086656c314
Mikel <mikelgaldosv@gmail.com>
2023-01-27 05:03:14 +0100

    Donde Estamos page created

---
+++ pages/dEstamos.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/layout";
+import styles from "../styles/dEstamos.module.css"
+
+export default function dEstamos() {
+    return(
+        <Layout home>
+            <div className={styles.container}>this is donde estamos</div>
+        </Layout>
+    )
+}
