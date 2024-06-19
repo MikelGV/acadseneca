75d6487c4f69149dad01033ebc22729d92b33b1c
MikelGv <mikelgaldosv@gmail.com>
2024-06-19 06:17:30 +0200

    yarn.lock deleted and img changed to Img

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -4,11 +4,11 @@ export default function About() {
     return(
         <div className={styles.aboutWrapper}>
             <div className={styles.aboutLeft}>
-              <img className={styles.leftImg} src="/img/aboutTheFormationOfGroups.jpg" alt="" />
+              <img className={styles.leftImg} src="/Img/aboutTheFormationOfGroups.jpg" alt="" />
               <p className={styles.leftText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis, minima ipsum dolore in, pariatur quaerat placeat error inventore natus dolor harum! Debitis recusandae consequuntur earum! Hic odio saepe in?</p>
             </div>
             <div className={styles.aboutRight}>
-              <img className={styles.rightImg} src="/img/aboutTheTeachers.jpg" alt="" />
+              <img className={styles.rightImg} src="/Img/aboutTheTeachers.jpg" alt="" />
               <p className={styles.rightText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis, minima ipsum dolore in, pariatur quaerat placeat error inventore natus dolor harum! Debitis recusandae consequuntur earum! Hic odio saepe in?</p>
             </div>
         </div>
