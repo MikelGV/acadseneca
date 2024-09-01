673b1651248d3f4d85ac445dd835461424b5817e
MikelGv <mikelgaldosv@gmail.com>
2024-09-02 00:00:26 +0200

    contact styles added

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -2,6 +2,7 @@
 
 import { useForm } from "react-hook-form"
 import { sendEmail } from "../../utils/send-email";
+import styles from "../contact/contact.module.scss";
 
 export type FormData = {
     name: string,
@@ -18,23 +19,17 @@ export default function Contact() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="name"> Full Name </label>
-                <input type="text" placeholder="Full Name" {...register("name", {required: true})} />
-            </div>
-            <div>
-                <label htmlFor="name"> Email Address </label>
-                <input type="email" placeholder="example@example.com" {...register("email", {required: true})} />
-            </div>
-            <div>
-                <label htmlFor="message"> Message </label>
-                <textarea rows={4} placeholder="Type your message" {...register("message", {required:true})}></textarea>
-            </div>
+        <div className={styles.FormWrapper}>
+            <h2 className={styles.ContactUs}>Contact Us</h2>
+            <form className={styles.Form} onSubmit={handleSubmit(onSubmit)}>
+                <input className={styles.Name} type="text" placeholder="Full Name" {...register("name", {required: true})} />
+                <input className={styles.Email} type="email" placeholder="example@example.com" {...register("email", {required: true})} />
+                <textarea className={styles.Message} rows={4} placeholder="Type your message" {...register("message", {required:true})}></textarea>
 
-            <div>
-                <button>Submit</button>
-            </div>
-        </form>
+                <div>
+                    <button className={styles.Button}>Submit</button>
+                </div>
+            </form>
+        </div>
     )
 }
